b9862668f37d38af99ae1b450f2d1cd42e416238
Peter Barth <osm-tileserver@won2.de>
2011-12-30 22:25:30 +0100

    added initial version of web-folder - still has to be synced to the server's version

---
+++ web/update.php
@@ -0,0 +1,24 @@
+<? $x = $_GET["x"]; $y = $_GET["y"]; ?>
+
+<?
+$myfile = "output/render_tasks";
+$fh = fopen($myfile, 'a') or die("can't open file!");
+$toWrite = $x . " " . $y . "\n"; 
+fwrite($fh, $toWrite);
+?>
+
+<html>
+<head><title>Regenerate Tile <?=$x?>/<?=$y?></title></head>
+<body>
+
+<p>
+Request sent. It will take some hours until tile <?=$x?>/<?=$y?> will be rerendered.
+Please be patient.
+</p>
+
+<p>
+<a href="tiles.php?x=<?=$x?>&y=<?=$y?>">Back</a>
+</p>
+
+</body>
+</html>
